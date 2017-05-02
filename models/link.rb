d108e001a4ac3e03fd0ed1a7533796135493b08d
Adam Snow <adamjohnsnow@icloud.com>
2017-05-02 18:23:35 +0100

    heroku added

--- models/link.rb
+++ models/link.rb
@@ -11,9 +11,9 @@ end
 if ENV['RACK_ENV'] == 'test'
   @database = "postgres://localhost/bookmark_manager_test"
 else
-  @database = "postgres://pnpbtwdieojoss:25055d049afe74def87b524d8cfd8ee3a0dda25d35ee8ae10f7808e9b1538498@ec2-54-83-25-217.compute-1.amazonaws.com:5432/dboj9g06brrgh2"
+  @database = "postgres://localhost/bookmark_manager_development"
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setupDataMapper.setup(:default, ENV['DATABASE_URL'] || @database)
 DataMapper.finalize
 DataMapper.auto_upgrade!
